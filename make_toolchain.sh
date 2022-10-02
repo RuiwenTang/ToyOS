7fcb2ad1c0aef88c99b620deb308826900f88a3e
RuiwenTang <tangruiwen1989@gmail.com>
2022-10-02 23:53:22 +0800

    basic project

---
+++ make_toolchain.sh
@@ -0,0 +1,58 @@
+#! /bin/bash
+
+export PREFIX="$HOME/osdev/toolchain"
+export TARGET=i686-floppy-elf
+export PATH="$PREFIX/bin:$PATH"
+export CROSS_SYSROOT="$HOME/osdev/sysroot"
+
+mkdir -p toolchain
+
+cd toolchain
+
+TOOLCHAIN_ROOT=$pwd
+
+if [ ! -f binutils-2.34.tar.gz ]
+then
+    wget http://ftp.gnu.org/gnu/binutils/binutils-2.34.tar.gz
+fi
+
+if [ ! -d binutils-2.34 ]
+then
+tar xvfz binutils-2.34.tar.gz
+fi
+
+if [ ! -d build_binutils ]
+then
+
+mkdir build_binutils
+cd build_binutils
+../binutils-2.34/configure --target=$TARGET --prefix="$PREFIX" --with-sysroot=$CROSS_SYSROOT --disable-werror
+make
+make install
+cd ../
+fi
+
+if [ ! -f gcc-9.3.0.tar.xz ]
+then
+    wget ftp://ftp.gnu.org/gnu/gcc/gcc-9.3.0/gcc-9.3.0.tar.xz
+fi
+
+if [ ! -d gcc-9.3.0 ]
+then
+    tar xvfz gcc-9.3.0.tar.xz
+    cd gcc-9.3.0
+    ./contrib/download_prerequisites
+    cd ../
+fi
+
+if [ ! -d build_gcc ]
+then
+    mkdir -p build_gcc
+    cd build_gcc
+    echo $pwd
+    ../gcc-9.3.0/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=c,c++
+    make all-gcc
+    make all-target-libgcc
+    make install-gcc
+    make install-target-libgcc
+fi
