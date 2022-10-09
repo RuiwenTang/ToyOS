7cceb4c9f4a621ea08e3774de4f70c53f2f76ece
RuiwenTang <tangruiwen1989@gmail.com>
2022-10-09 11:26:30 +0800

    add bochs run script

---
+++ run_bochs.py
@@ -0,0 +1,6 @@
+import subprocess
+
+if __name__ == '__main__':
+    subprocess.call(['echo', 'run qemu'])
+    subprocess.call(['meson', 'compile', '-C', 'build', 'tools/gen_floppy'])
+    subprocess.call(['bochs', '-f', 'bochsrc.txt', '-q'])
