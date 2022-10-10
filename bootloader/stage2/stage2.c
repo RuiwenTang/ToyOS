55b2987cbb7949aedbcb733b4a17bea847ffda09
RuiwenTang <tangruiwen1989@gmail.com>
2022-10-10 17:00:55 +0800

    Print string in graphic mode with bitmap font

--- bootloader/stage2/stage2.c
+++ bootloader/stage2/stage2.c
@@ -1,4 +1,6 @@
 
+#include "font8x8.h"
+
 typedef unsigned short uint16;
 typedef unsigned char uint8;
 typedef unsigned int uint32;
@@ -46,6 +48,49 @@ struct vbe_mode_info_structure {
   uint8 reserved1[206];
 } __attribute__((packed));
 
+void clear_screen(uint8 *addr, uint16 width, uint16 height, uint16 pitch) {
+  for (uint16 x = 0; x < width; x++) {
+    for (uint16 y = 0; y < height; y++) {
+      uint8 *line = addr + pitch * y;
+
+      uint8 *pix = line + x * 3;
+      pix[0] = 0x0;
+      pix[1] = 0x0;
+      pix[2] = 0x0;
+    }
+  }
+}
+
+uint8 *g_fb = 0;
+uint16 g_width = 0;
+uint16 g_height = 0;
+uint16 g_pitch = 0;
+
+void put_pixel(uint32 color, uint16 x, uint16 y) {
+  uint8 *line = g_fb + g_pitch * y;
+
+  uint8 *pix = line + x * 3;
+
+  pix[0] = color & 0xFF;
+  pix[1] = (color >> 8) & 0xFF;
+  pix[2] = (color >> 16) & 0xFF;
+}
+
+void put_char(char c, uint16 x, uint16 y) {
+  uint32 pix = 0x00FFFFFF;
+
+  uint8 *bitmap = (uint8 *)font8x8_basic[c];
+
+  for (int i = 0; i < 8; i++) {
+    for (int j = 0; j < 8; j++) {
+      int set = bitmap[i] & (1 << j);
+      if (set) {
+        put_pixel(pix, x + j, y + i);
+      }
+    }
+  }
+}
+
 void stage2_main(void *info) {
   struct vbe_mode_info_structure *vbe_info =
       (struct vbe_mode_info_structure *)info;
@@ -53,6 +98,25 @@ void stage2_main(void *info) {
   int *p = (int *)0x9000;
 
   *p = vbe_info->bpp;
+
+  g_fb = (uint8 *)vbe_info->framebuffer;
+  g_width = vbe_info->width;
+  g_height = vbe_info->height;
+  g_pitch = vbe_info->pitch;
+
+  clear_screen((uint8 *)vbe_info->framebuffer, vbe_info->width,
+               vbe_info->height, vbe_info->pitch);
+
+  int x = 0;
+  int y = 2;
+
+  char msg[] = "Print In Protect Mode";
+
+  int i = 0;
+  for (i; i < sizeof(msg); i++) {
+    put_char(msg[i], x, y);
+    x += 8;
+  }
 
   return;
 }
