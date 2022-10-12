f7857b3ce5b429063f9c3a2e44bb2349de4420e2
RuiwenTang <tangruiwen1989@gmail.com>
2022-10-12 12:04:13 +0800

    printf in bootloader

---
+++ bootloader/stage2/printf.h
@@ -0,0 +1,8 @@
+#ifndef BOOT_PRINTF_H
+#define BOOT_PRINTF_H
+
+// simple implement just use for bootloader
+// https://github.com/limine-bootloader/limine/blob/trunk/common/lib/print.s2.c
+void printf(const char *, ...);
+
+#endif // BOOT_PRINT_F
--- bootloader/stage2/stage2.c
+++ bootloader/stage2/stage2.c
@@ -1,5 +1,6 @@
 
 #include "disk/fat.h"
+#include "printf.h"
 #include "screen/screen.h"
 
 void stage2_main(void *info, uint16_t boot_drive) {
@@ -7,9 +8,7 @@ void stage2_main(void *info, uint16_t boot_drive) {
   screen_init((struct vbe_mode_info_structure *)info);
   screen_clear();
 
-  char msg[] = "Print In Protect Mode\n";
-
-  screen_print(msg, sizeof(msg), SCREEN_COLOR_RED);
+  printf("Print In Protect Mode\n");
 
   if (fat_init(boot_drive)) {
     screen_print("fat init failed!", 16, SCREEN_COLOR_RED);
