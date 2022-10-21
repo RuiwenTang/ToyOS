15524ccf31d3fee6e9d8aa6fd6b026395fe4a209
RuiwenTang <tangruiwen1989@gmail.com>
2022-10-21 18:12:11 +0800

    Change kernel main to cpp

--- kernel/src/kprintf.h
+++ kernel/src/kprintf.h
@@ -1,8 +1,16 @@
 #ifndef KERNEL_PRINTF_H
 #define KERNEL_PRINTF_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 // simple implement just used in kernel for debug
 // https://github.com/limine-bootloader/limine/blob/trunk/common/lib/print.s2.c
 void kprintf(const char *, ...);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif  // KERNEL_PRINTF_H
--- kernel/src/screen/screen.h
+++ kernel/src/screen/screen.h
@@ -3,6 +3,10 @@
 
 #include <boot/toy_boot.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #define SCREEN_COLOR_WHITE 0xffffff
 
 #define SCREEN_COLOR_RED 0xff0000
@@ -14,5 +18,9 @@ void screen_set_color(uint32_t color);
 void screen_clear();
 
 void screen_print(char* str, uint32_t len, uint32_t color);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif  // KERNEL_SCREEN_SCREEN_H
