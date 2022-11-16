2b9e866351425e605552c8ada7e3901a1b924dfa
RuiwenTang <tangruiwen1989@gmail.com>
2022-11-16 17:42:34 +0800

    change to use os specify gcc toolchain

--- make_toolchain.sh
+++ make_toolchain.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 export PREFIX="$HOME/osdev/toolchain"
-export TARGET=i686-toy-elf
+export TARGET=i686-toy
 export PATH="$PREFIX/bin:$PATH"
 export CROSS_SYSROOT="$HOME/osdev/sysroot"
 
@@ -11,37 +11,44 @@ cd toolchain
 
 TOOLCHAIN_ROOT=$pwd
 
-if [ ! -f binutils-2.34.tar.gz ]
+if [ ! -f binutils-2.39.tar.gz ]
 then
-    wget http://ftp.gnu.org/gnu/binutils/binutils-2.34.tar.gz
+    wget http://ftp.gnu.org/gnu/binutils/binutils-2.39.tar.gz
 fi
 
-if [ ! -d binutils-2.34 ]
+if [ ! -d binutils-2.39 ]
 then
-tar xvfz binutils-2.34.tar.gz
+tar xvfz binutils-2.39.tar.gz
+fi
+
+if [ -d binutils-2.39 ]
+then
+    cd binutils-2.3.9
+    patch -p1 < ../../patchs/binutils-2.93.patch
+    cd ../
 fi
 
 if [ ! -d build_binutils ]
 then
-
-mkdir build_binutils
-cd build_binutils
-../binutils-2.34/configure --target=$TARGET --prefix="$PREFIX" --with-sysroot=$CROSS_SYSROOT --disable-werror
-make
-make install
-cd ../
+    mkdir build_binutils
+    cd build_binutils
+    ../binutils-2.39/configure --target=$TARGET --prefix="$PREFIX" --with-sysroot=$CROSS_SYSROOT --disable-werror --disable-gdb
+    make
+    make install
+    cd ../
 fi
 
-if [ ! -f gcc-9.3.0.tar.xz ]
+if [ ! -f gcc-12.2.0.tar.xz ]
 then
-    wget ftp://ftp.gnu.org/gnu/gcc/gcc-9.3.0/gcc-9.3.0.tar.xz
+    wget ftp://ftp.gnu.org/gnu/gcc/gcc-12.2.0/gcc-12.2.0.tar.xz
 fi
 
-if [ ! -d gcc-9.3.0 ]
+if [ ! -d gcc-12.2.0 ]
 then
-    tar xvfz gcc-9.3.0.tar.xz
-    cd gcc-9.3.0
+    tar xvfz gcc-12.2.0.tar.xz
+    cd gcc-12.2.0
     ./contrib/download_prerequisites
+    patch -p1 < ../../patchs/gcc-12.2.0.patch
     cd ../
 fi
 
@@ -50,7 +57,7 @@ then
     mkdir -p build_gcc
     cd build_gcc
     echo $pwd
-    ../gcc-9.3.0/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=c,c++
+    ../gcc-12.2.0/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=c,c++ --enable-shared
     make all-gcc
     make all-target-libgcc
     make install-gcc
