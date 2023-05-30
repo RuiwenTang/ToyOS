563c25957ac4b8aad390acc395895fb546ffcc7d
RuiwenTang <tangruiwen1989@gmail.com>
2023-05-30 23:21:42 +0800

    format ext4 file img

--- tools/ext4_host_backend.hpp
+++ tools/ext4_host_backend.hpp
@@ -11,14 +11,28 @@ class Ext4File {
   Ext4File(std::string path);
   ~Ext4File();
 
+  bool IsValid() const;
+
   bool Format(const std::string& type);
 
   bool Install(const std::string& src, const std::string& dst);
 
+  void Seekg(uint64_t offset);
+
+  void Read(char* buf, uint64_t size);
+
+  void Seekp(uint64_t offset);
+
+  void Write(char* buf, uint64_t size);
+
  private:
   bool Init();
+
+  bool LoadPartionDev();
 
  private:
   std::string m_file_path;
   std::fstream m_file_stream = {};
+
+  ext4_blockdev m_sub_dev = {};
 };
