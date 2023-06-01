3f877bddf2c34b883b7c1a8da4f868fee34162f7
RuiwenTang <tangruiwen1989@gmail.com>
2023-06-01 23:41:22 +0800

    run qemu target

--- run_bochs.py
+++ run_bochs.py
@@ -2,9 +2,4 @@ import subprocess
 
 if __name__ == '__main__':
     subprocess.call(['echo', 'run bochs'])
-    ret = subprocess.call(
-        ['meson', 'compile', '-C', 'build', 'tools/install_limine'])
-    if ret != 0:
-        print("build failed")
-        exit()
     subprocess.call(['bochs', '-f', 'bochsrc.txt', '-q'])
--- run_qemu.py
+++ run_qemu.py
@@ -3,14 +3,9 @@ import sys
 
 if __name__ == '__main__':
     subprocess.call(['echo', 'run qemu'])
-    ret = subprocess.call(
-        ['meson', 'compile', '-C', 'build', 'tools/install_limine'])
-    if ret != 0:
-        print("build failed")
-        exit()
     if len(sys.argv) >= 2 and sys.argv[1] == '-d':
         subprocess.call(['qemu-system-i386', '-hda',
-                         'build/tools/disk.img', '-monitor', 'stdio', '-s', '-S'])
+                         sys.argv[1], '-monitor', 'stdio', '-s', '-S'])
     else:
         subprocess.call(['qemu-system-i386', '-hda',
-                         'build/tools/disk.img', '-monitor', 'stdio'])
+                         sys.argv[1], '-monitor', 'stdio'])
