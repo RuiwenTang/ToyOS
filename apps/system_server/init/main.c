d277f5bf020f6b2c359d1e0422d63be8ae515fee
RuiwenTang <tangruiwen1989@gmail.com>
2023-09-05 15:10:35 +0800

    libc support malloc

--- apps/system_server/init/main.c
+++ apps/system_server/init/main.c
@@ -1,5 +1,6 @@
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, const char** argv) {
   char buf[50];
@@ -7,9 +8,20 @@ int main(int argc, const char** argv) {
 
   double a = cos(0.3);
 
+  void* ptr = malloc(20);
+  void* ptr1 = malloc(20);
+
   sprintf(buf, "a = %lf\n", a);
 
   printf("cos(0.3) = %lf\n", a);
+
+  printf("ptr from malloc is %p \n", ptr);
+  printf("ptr1 from malloc is %p \n", ptr1);
+
+  free(ptr);
+
+  ptr = malloc(20);
+  printf("ptr again from malloc is %p \n", ptr);
 
   return 0;
 }
